fc229e310153f3167353655042019e1dfb0655a8
jonathanadam <jonathan_adam@brown.edu>
2015-06-09 09:30:12 -0700

    seeing whether googleStorage is what i want it to be

--- test/test_googleStorage.js
+++ test/test_googleStorage.js
@@ -6,20 +6,21 @@ var expect = require('expect.js')
 // Our Modules
 /////////////////////////
 var storageManager = require('../')
+console.log(storageManager)
 
 //var config = require('c:/temp/config.js')
 
 
 // Tests
 /////////////////////////
-describe('storageManger: googleStorage', function()
+/*describe('storageManger: googleStorage', function()
 {
 
 it('should load', function(done)
 {
 	console.log(storageManager)
 	var googleStorage = storageManger.start('google', "lala", done)
-})
+}) */
 
 
 
